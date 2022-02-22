1fd9fe13c588fde59718976d030d20af44782ca5
Misha <@mikhail_ogar>
2022-02-22 04:41:35 +0300

    Add

---
+++ src/Rabot.java
@@ -0,0 +1,30 @@
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Rabot {
+    public static void main(String[] args) throws  Exception{
+        FileReader fileAirports = new FileReader("C:/airports.dat");
+        Scanner scan = new Scanner(fileAirports);
+
+
+        System.out.println("Введите строку:");
+        String info = scan.nextLine();
+        String regex = "[^\r\n]*" + info + "*[^\r\n]*";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(info);
+        long AtTheStart = System.currentTimeMillis();
+        int count = 0;
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            System.out.println(fileAirports.substring(start, end));
+            count += 1;
+        }
+        long AtTheEnd = System.currentTimeMillis() - AtTheStart;
+        System.out.println("Количество найденных строк: " + count + " Время, затраченное на поиск: " + AtTheEnd + " мс");
+        scan.close();
+    }
+}
